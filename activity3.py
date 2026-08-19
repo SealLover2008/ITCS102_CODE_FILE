2870105f01887b4fa328fcc76d917e6322fcd66e
SealLover2008 <lloydfritz14@gmail.com>
2026-08-19 17:42:37 +0800

    More uniform
    tweaked

--- activity3.py
+++ activity3.py
@@ -24,4 +24,28 @@ isTall= False
 isCool= False
 isFast= True
 
-print ("I am " + name + " I have " + amount_of_money + " I am " + height + " and " + weight + " also my age is " + age)
+print("=== PERSONAL INFORMATION ===")
+print("Name:", name)
+print("Age:", age)
+print("Height:", height, "cm")
+print("Weight:", weight, "kg")
+print("Amount of Money:", amount_of_money)
+
+print("=== OTHER INFORMATION ===")
+print("Blood Pressure:", bp)
+print("Battery Percentage:", batteryPercntage, "%")
+print("Liters of Water:", littersOfWater)
+print("Shoe Size:", shoeSize)
+
+print("=== FAMILY & PET ===")
+print("Maiden:", maiden)
+print("Father:", father)
+print("Cat:", cat)
+
+print("=== STATUS ===")
+print("Rich:", isRich)
+print("Tall:", isTall)
+print("Cool:", isCool)
+print("Fast:", isFast)
+
+print("My name is", name, "and I am", age, "years old. I am", height, "cm tall and weigh", weight, "kg. I have", amount_of_money, "pesos. My BP is", bp, "and my battery is", batteryPercntage, "%. I drink", littersOfWater, "liters of water and my shoe size is", shoeSize, ". My father is", father, "and my cat is", cat, ". Rich:", isRich, "Tall:", isTall, "Cool:", isCool, "Fast:", isFast)
